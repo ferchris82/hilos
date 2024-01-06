61ee2672b647d062a5b9b9413b323367f2a67c2a
Christian <ferchris82@hotmail.com>
2024-01-06 17:52:32 -0500

    Versión 0.1

---
+++ src/org/chrisferdev/hilos/ejemplos/EjemploInterfaceRunnable.java
@@ -0,0 +1,13 @@
+package org.chrisferdev.hilos.ejemplos;
+
+import org.chrisferdev.hilos.ejemplos.runnable.ViajeTarea;
+
+public class EjemploInterfaceRunnable {
+    public static void main(String[] args) {
+
+        new Thread(new ViajeTarea("Isla de Pascua")).start();
+        new Thread(new ViajeTarea("Robinson Crusoe")).start();
+        new Thread(new ViajeTarea("Juan Fernandez")).start();
+        new Thread(new ViajeTarea("Isla de Chiloe")).start();
+    }
+}
