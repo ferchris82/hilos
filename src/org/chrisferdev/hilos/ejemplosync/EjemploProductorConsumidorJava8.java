4e4812a09721ab553eaabb7c6a613eb414125f1f
Christian <ferchris82@hotmail.com>
2024-01-08 13:12:35 -0500

    Versión 1.0

---
+++ src/org/chrisferdev/hilos/ejemplosync/EjemploProductorConsumidorJava8.java
@@ -0,0 +1,29 @@
+package org.chrisferdev.hilos.ejemplosync;
+
+import org.chrisferdev.hilos.ejemplosync.runnable.Consumidor;
+import org.chrisferdev.hilos.ejemplosync.runnable.Panadero;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class EjemploProductorConsumidorJava8 {
+    public static void main(String[] args) {
+        Panaderia p = new Panaderia();
+
+        new Thread(() -> {
+            for(int i = 0; i < 10; i++){
+                p.hornear("Pan n°: " + i);
+                try {
+                    Thread.sleep(ThreadLocalRandom.current().nextInt(500, 2000));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        new Thread(() -> {
+            for(int i = 0; i <10; i++){
+                p.consumir();
+            }
+        }).start();
+    }
+}
